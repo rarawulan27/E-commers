3d13e01bd76a902821e4a7e55bdb260a33421f27
Rara Wulandari <rarawulandari2000@gmail.com>
2025-06-27 19:52:45 +0700

    Update clean_user_data.sql

--- clean_user_data.sql
+++ clean_user_data.sql
@@ -19,15 +19,7 @@ FROM `molten-castle-460910-e1.theLook_eCommerce.User_id`
 WHERE created_at_wib <= '2025-06-01 00:00:00';
 
 
-----Buat tau kalok performa pengiriman dari id order/performa pengiriman berapa lama------
-SELECT
-  order_id,
-  TIMESTAMP_DIFF(shipped_at, created_at, DAY) AS days_to_ship,
-  TIMESTAMP_DIFF(delivered_at, shipped_at, DAY) AS days_to_deliver
-FROM
-bigquery-public-data.thelook_ecommerce.orders
-WHERE
-  shipped_at IS NOT NULL AND delivered_at IS NOT NULL;
+-
 ---Buat tau kalok tipe event dan total event-----
 SELECT 
   event_type,
