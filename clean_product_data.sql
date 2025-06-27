71d87425d4dd21efc3a40bba7545ccff69d042b9
Rara Wulandari <rarawulandari2000@gmail.com>
2025-06-27 11:20:49 +0700

    Update clean_product_data.sql

--- clean_product_data.sql
+++ clean_product_data.sql
@@ -8,4 +8,13 @@ GROUP BY
   brand_bersih
 ORDER BY
 total_retail DESC; 
-
+----- untuk mengetahui jumlah kategori brand dan item brand----
+SELECT 
+   brand_bersih,
+  COUNT (DISTINCT category) AS kategori_produk,
+  COUNT(*) AS jumlah_produk,
+FROM `molten-castle-460910-e1.theLook_eCommerce.Product`
+GROUP BY 
+brand_bersih
+ORDER BY
+jumlah_produk DESC;
