7db287ffedddd14751feb7bbfb638c03f21a16ca
Rara Wulandari <rarawulandari2000@gmail.com>
2025-06-28 11:36:00 +0700

    Update clean_order_order_item_data.sql

--- clean_order_order_item_data.sql
+++ clean_order_order_item_data.sql
@@ -7,3 +7,15 @@ FROM
 bigquery-public-data.thelook_ecommerce.orders
 WHERE
   shipped_at IS NOT NULL AND delivered_at IS NOT NULL;
+-----Buat tau loyal customer----
+SELECT
+  first_name
+  user_id,
+  COUNT(order_id) AS total_orders,
+  CASE 
+    WHEN COUNT(order_id) = 1 THEN 'New Customer'
+    WHEN COUNT(order_id) <= 3 THEN 'Returning Customer'
+    ELSE 'Loyal Customer'
+  END AS customer_segment
+ FROM `molten-castle-460910-e1.Kpi_clean_data.v_clean_order_data` 
+ GROUP BY user_id 
