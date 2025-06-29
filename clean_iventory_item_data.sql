8087bc7b69219c5a65612a04a800d367901e6c36
Rara Wulandari <rarawulandari2000@gmail.com>
2025-06-29 19:46:05 +0700

    Update clean_iventory_item_data.sql

--- clean_iventory_item_data.sql
+++ clean_iventory_item_data.sql
@@ -13,3 +13,21 @@ SELECT
   DATETIME(TIMESTAMP(created_at), 'Asia/Jakarta') AS created_at_wib,
   DATETIME(TIMESTAMP(sold_at), 'Asia/Jakarta') AS sold_at_wib
  FROM `bigquery-public-data.thelook_ecommerce.inventory_items` LIMIT 1000
+----Sales Trend Over Time------
+SELECT 
+  o.order_id,
+  o.status_order,
+  o.shipped_at,
+  p.retail_price_product
+FROM 
+  `molten-castle-460910-e1.theLook_eCommerce.Order` AS o
+JOIN 
+  `molten-castle-460910-e1.theLook_eCommerce.Order Item ` AS oi
+  ON o.order_id = oi.order_id
+JOIN 
+  `molten-castle-460910-e1.theLook_eCommerce.Product` AS p
+  ON oi.product_id = p.id_product
+WHERE 
+  o.status_order = 'Shipped'
+  AND o.shipped_at IS NOT NULL
+
